b32a846ac08a991932963198465577cd16c6f79c
Silje32 <msh070425@gmail.com>
2025-08-19 12:44:40 +0200

    Moved my useState

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import cookie from "/cookie-1.png";
 
 export default function Button() {
-  <button onClick={() => setCount((count) => count + 1)}>
-    count is {count}
-    <img src="cookie-1.png" alt="Image of a Cookie"></img>
-  </button>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <button onClick={() => setCount((count) => count + 1)}>
+      count is {count}
+      <img src="cookie-1.png" alt="Image of a Cookie"></img>
+    </button>
+  );
 }
